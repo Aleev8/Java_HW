86c1bf5ba3d49a246269d4ddfffa35fc6cb5df64
Aleev_nb <aleev.alexandr@gmail.com>
2023-03-14 21:12:59 +0600

    Create new class "task3" in package "sem2", and solved task3

---
+++ src/sem2/task3.java
@@ -0,0 +1,35 @@
+package sem2;
+
+// Напишите метод, который принимает на вход строку (String) и определяет является
+// ли строка палиндромом (возвращает boolean значение). Без встр. функций
+
+
+public class task3 {
+
+    public static boolean isPalindrome(String str)
+    {
+        if (str == null) {
+            return false;
+        }
+        for (int i = 0, j = str.length() - 1; i < j; i++, j--)
+        {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main (String[] args)
+    {
+        String str = "yessey";
+
+        if (isPalindrome(str)) {
+            System.out.println("Yes, Palindrome");
+        }
+        else {
+            System.out.println("Not Palindrome");
+        }
+    }
+
+}
